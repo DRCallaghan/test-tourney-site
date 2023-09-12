47aad7de21c2ed1fb812b57324e00647c347329e
DRCallaghan <dennis.callaghan87@gmail.com>
2023-09-12 14:58:18 -0400

    layout redo

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,16 @@
+import OsuProvider from "next-auth/providers/osu";
+import GitHubProvider from "next-auth/providers/github";
+
+export const nextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        OsuProvider({
+            clientId: process.env.OSU_CLIENT_ID,
+            clientSecret: process.env.OSU_CLIENT_SECRET
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+    ]
+};
